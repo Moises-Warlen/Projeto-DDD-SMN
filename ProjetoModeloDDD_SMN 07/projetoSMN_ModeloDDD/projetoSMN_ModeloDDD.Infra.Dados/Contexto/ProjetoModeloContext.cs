2d087ad8051ddf348220389d08ae80c0d3e4895b
Moises Warlen <moisesfatecads@gmail.com>
2021-02-02 14:10:35 -0300

    ConnectionString criada e habilitar a migration

---
+++ ProjetoModeloDDD_SMN 07/projetoSMN_ModeloDDD/projetoSMN_ModeloDDD.Infra.Dados/Contexto/ProjetoModeloContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using projetoSMN_ModeloDDD.Dominio.Entidades;
+
+namespace projetoSMN_ModeloDDD.Infra.Dados.Contexto
+{
+    public class ProjetoModeloContext:DbContext
+    {
+        public ProjetoModeloContext()
+            :base("projetoModeloDDD")
+        {
+                
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<TipoUsuario> TipoUsuarios { get; set; }
+        public DbSet<Imagem> Imagems { get; set; }
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<StatusTarefa> StatusTarefas { get; set; }
+
+    }
+}
