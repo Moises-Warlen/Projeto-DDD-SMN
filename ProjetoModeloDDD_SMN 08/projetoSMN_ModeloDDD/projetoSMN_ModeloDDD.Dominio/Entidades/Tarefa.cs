1dd3a866431ffa52d24afe0a9f7e0d5f06850017
Moises Warlen <moisesfatecads@gmail.com>
2021-02-03 09:36:20 -0300

    criando os relacionamentos

---
+++ ProjetoModeloDDD_SMN 08/projetoSMN_ModeloDDD/projetoSMN_ModeloDDD.Dominio/Entidades/Tarefa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoSMN_ModeloDDD.Dominio.Entidades
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int StatusTarefaId { get; set; }
+        public string Descricao { get; set; }
+        DateTime DataInicio { get; set; }
+        DateTime DataFim { get; set; }
+        public string URL { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+        public virtual StatusTarefa StatusTarefa { get; set; }
+    }
+}
